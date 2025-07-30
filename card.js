cc571cb5c3a536d23417af7d9a1b9507d5d027a6
Gaelle Be <ventolinester@gmail.com>
2025-07-30 15:17:24 +0200

    drag controls

--- card.js
+++ card.js
@@ -1,102 +1,152 @@
-import * as THREE from 'three';
+import * as THREE from "three";
 //import  {gsap} from 'gsap';
 
-const cardsHolder  = new THREE.Object3D();
+const cardsHolder = new THREE.Object3D();
 
-export default class Cards{
-  
-
- // 3D object
-  makeCard  = (nb) => {
-    const geometry = new THREE.PlaneGeometry( 1.35, 1 );
+export default class Cards {
+  // 3D object
+  makeCard = (nb) => {
+    const geometry = new THREE.PlaneGeometry(1.35, 1);
     geometry.computeBoundingBox();
-    let colorRand =  new THREE.Color( Math.random(), Math.random(), Math.random() );
-    let material = new THREE.MeshBasicMaterial( { color: /*0xffff00*/  colorRand, side: THREE.DoubleSide} );
+    let colorRand = new THREE.Color(
+      Math.random(),
+      Math.random(),
+      Math.random()
+    );
+    let material = new THREE.MeshBasicMaterial({
+      color: /*0xffff00*/ colorRand,
+      side: THREE.DoubleSide,
+    });
     material.userData.no = nb;
-    const plane = new THREE.Mesh( geometry, material );
+    const plane = new THREE.Mesh(geometry, material);
     return plane;
-}
+  };
 
+  //container
 
-//container
-
-  makeCards =(no) => {
-   // const cardsHolder  = new THREE.Object3D();
+  makeCards = (no) => {
+    // const cardsHolder  = new THREE.Object3D();
     let maxWidth = 0;
     let maxHeight = 0;
-    
-    for(let i = 0; i<no; i++){
-        let newCard = this.makeCard(i);
-        //position the cards
-        cardsHolder.add(newCard);
-        newCard.position.x += (newCard.geometry.parameters.width +.1) *( i%5);
-        newCard.position.y -= (newCard.geometry.parameters.height +.1) *( Math.floor(i/5));
 
-        //INTERACTION
-//newCard.addEventListener('hovered')
-//clicked
-//dragged
-//dropped >  change position
+    for (let i = 0; i < no; i++) {
+      let newCard = this.makeCard(i);
+      //position the cards
+      cardsHolder.add(newCard);
+      newCard.position.x += (newCard.geometry.parameters.width + 0.1) * (i % 5);
+      newCard.position.y -=
+        (newCard.geometry.parameters.height + 0.1) * Math.floor(i / 5);
+
+      //INTERACTION
+      //newCard.addEventListener('hovered')
+      //clicked
+      //dragged
+      //dropped >  change position
     }
 
-//CENTER CARDS
-    maxWidth = (cardsHolder.children[0].geometry.parameters.width +.1) * 5 -.1;
-    maxHeight = (cardsHolder.children[0].geometry.parameters.height ) * Math.floor(no/5) +.1;
-    cardsHolder.position.x -= maxWidth/2;
-    cardsHolder.position.y += maxHeight/2;
-    
-    console.log(cardsHolder)
+    //CENTER CARDS
+    maxWidth =
+      (cardsHolder.children[0].geometry.parameters.width + 0.1) * 5 - 0.1;
+    maxHeight =
+      cardsHolder.children[0].geometry.parameters.height * Math.floor(no / 5) +
+      0.1;
+    cardsHolder.position.x -= maxWidth / 2;
+    cardsHolder.position.y += maxHeight / 2;
 
-return cardsHolder; 
- }
+    console.log(cardsHolder);
 
+    return cardsHolder;
+  };
 
- hoverCards = (no) =>{
-    console.log('hovering',no)
-    cardsHolder.children.forEach(card =>{
-if( card.material.userData == no) {
-   // tween forward
-   gsap.to(card.position, {duration:.5, z:1} )
-   gsap.to(card.scale, {duration:.5, z:1.15} )
-}
-else{
-    if(card.position.z != 0){
+  hoverCards = (no) => {
+    console.log("hovering", no);
+    cardsHolder.children.forEach((card) => {
+      if (card.material.userData == no) {
+        // tween forward
+        gsap.to(card.position, { duration: 0.5, z: 1 });
+        gsap.to(card.scale, { duration: 0.5, z: 1.15 });
+      } else {
+        if (card.position.z != 0) {
+          //tween back
+          gsap.to(card.position, { duration: 0.5, z: 0 });
+          gsap.to(card.scale, { duration: 0.5, z: 1 });
+        }
+      }
+    });
+  };
+
+  unhoverCards = () => {
+    cardsHolder.children.forEach((card) => {
+      if (card.position.z != 0) {
         //tween back
-        gsap.to(card.position, {duration:.5, z:0} )
-        gsap.to(card.scale, {duration:.5, z:1} )
+        gsap.to(card.position, { duration: 0.5, z: 0 });
+        gsap.to(card.scale, { duration: 0.5, z: 1 });
+      }
+    });
+  };
+
+  returnCard = (no) => {
+    cardsHolder.children.forEach((card) => {
+      if (card.material.userData == no) {
+        //flip
+        gsap.to(card.rotation, {
+          duration: 0.5,
+          y: card.rotation.y > Math.PI ? 0 : Math.PI,
+          overwrite: false,
+        });
+      }
+    });
+  };
+
+  grab = (no) => {
+    //move card
+    cardsHolder.children.forEach((card) => {
+      if (card.material.userData == no) {
+        //get  offset
+        //  card.position =  mouse - offset
+      }
+    });
+  };
+
+  drop = (card) => {
+    console.log("drop card", card);
+    // get position
+    let dropSpot = card.position;
+
+    console.log("card.position", card.position);
+    // console.log("card.height", card.geometry.parameters.height);
+
+    //consider a 5 x 2 grid
+    //  get closest spot on the grid
+    let col = Math.min(
+      4,
+      Math.floor(card.position.x / card.geometry.parameters.width)
+    );
+    col = Math.max(col, 0);
+
+    let raw = Math.max(
+      0,
+      Math.round(-card.position.y / card.geometry.parameters.height)
+    );
+
+    raw = raw > 1 ? 1 : raw;
+
+    console.log("on the grid ", col, "x", raw);
+
+    // swap
+    if (raw * 5 + col != card.material.userData.no) {
+      console.log("swap");
     }
+
+    console.log(raw * 5 + col, card.material.userData.no);
+
+    //replace cards
+    //TODO add tweens
+    card.position.y = -raw * card.geometry.parameters.height - raw * 0.1;
+    card.position.x = col * card.geometry.parameters.width + col * 0.1;
+
+    // tween both cards to new position
+  };
 }
 
-    })
-
- }
-
- unhoverCards = () => {
-    cardsHolder.children.forEach(card =>{
-            if(card.position.z != 0){
-                //tween back
-                gsap.to(card.position, {duration:.5, z:0} )
-                gsap.to(card.scale, {duration:.5, z:1} )
-            }
-            })
-        
- 
-}
-
-returnCard = (no)=>{
-    cardsHolder.children.forEach(card =>{
-        if( card.material.userData == no) {
-                //flip
-                gsap.to(card.rotation, {duration:.5, y: card.rotation.y > Math.PI ? 0 : Math.PI,
-                    overwrite:false
-                },
-             )
-            }
-        
-        
-            })
-        
-}
-}
-
-export {Cards}
+export { Cards };
--- scene.js
+++ scene.js
@@ -1,84 +1,130 @@
-import * as THREE from 'three';
-import Cards from '/card.js';
+import * as THREE from "three";
+import { DragControls } from "dragcontrols";
+import Cards from "/card.js";
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
 const cardsAmount = 10;
 let cardHighlighted = null;
+let dragging = false;
+let mousedowning = false;
+let intersects, controls;
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.setAnimationLoop( animate );
-document.body.appendChild( renderer.domElement );
-
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setAnimationLoop(animate);
+document.body.appendChild(renderer.domElement);
 
 //CARDS
 const cards = new Cards();
 const cardContainer = cards.makeCards(cardsAmount);
-scene.add( cardContainer );
+scene.add(cardContainer);
 
 camera.position.z = 5;
 
 const raycaster = new THREE.Raycaster();
 const pointer = new THREE.Vector2();
 
+controls = new DragControls(
+  cardContainer.children,
+  camera,
+  renderer.domElement
+);
+controls.rotateSpeed = 2;
+controls.addEventListener("drag", controlDragging);
+controls.addEventListener("dragend", onMouseDrop);
+controls.addEventListener("click", onMouseDown);
+
 function animate() {
+  //Mouse Interaction
+  raycaster.setFromCamera(pointer, camera);
+  intersects = raycaster.intersectObjects(cardContainer.children);
+  if (!!intersects.length > 0) {
+    document.body.style.cursor = "pointer";
 
-    //Mouse Interaction
-	raycaster.setFromCamera( pointer, camera );
-    const intersects = raycaster.intersectObjects( cardContainer.children );
-  if(!!intersects.length>0 ) 
-    { 
-      
-        document.body.style.cursor = 'pointer';
-        
-        if(intersects[0].object.material.userData != cardHighlighted)
-    {
-        cardHighlighted = intersects[0].object.material.userData;
-// hover action
-cards.hoverCards(cardHighlighted);
-//intersects[0].object.position.z = 1;
-        console.log(intersects[0].object.material.userData);
+    if (intersects[0].object.material.userData != cardHighlighted) {
+      cardHighlighted = intersects[0].object.material.userData;
+
+      // hover effect
+      cards.hoverCards(cardHighlighted);
+      console.log(intersects[0].object.material.userData);
     }
-       
-
-    } 
-
-    else{cards.unhoverCards()
-        document.body.style.cursor = 'default';
-    }
-  renderer.render( scene, camera );
-
+  } else {
+    cards.unhoverCards();
+    document.body.style.cursor = "default";
+  }
+  renderer.render(scene, camera);
 }
 
+window.onresize = () => {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.render(scene, camera);
+};
 
-window.onresize = () =>{
+function onPointerMove(event) {
+  // if (mousedowning) onMouseGrab(); // grab
+  // if (mousedowning && !dragging) {
+  //   dragging = true;
+  // }
 
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    camera.aspect = window.innerWidth / window.innerHeight
-    camera.updateProjectionMatrix();
-    renderer.render( scene, camera );
-    
+  // console.log("onPointerMove", event.type);
+
+  // calculate pointer position in normalized device coordinates
+  // (-1 to +1) for both components
+  pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
+  pointer.y = -(event.clientY / window.innerHeight) * 2 + 1;
 }
 
-
-function onPointerMove( event ) {
-
-	// calculate pointer position in normalized device coordinates
-	// (-1 to +1) for both components
-
-	pointer.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-	pointer.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
-
+function onMouseDown(event) {
+  //  mousedowning = true;
+  console.log("onMouseDown", event.type);
+  console.log("dragging", dragging);
+  // if (!dragging) {
+  cards.returnCard(cardHighlighted);
+  // }
 }
 
-function onMouseDown (event){
-    cards.returnCard(cardHighlighted);
+function onMouseUp(event) {
+  //mousedowning = false;
+  // dragging = false;
+  //  console.log("dragging", dragging);
+  console.log("onMouseUp", event.type);
 }
 
+function controlDragging(e) {
+  console.log("dragging controls", e);
+}
 
-function onMouseGrab(){}
+function onMouseGrab() {
+  // if (!dragging) return;
+  console.log("grabbing", intersects[0]);
+  console.log("dragging", dragging);
 
-window.addEventListener( 'pointermove', onPointerMove );
-window.addEventListener( 'click', onMouseDown );
-window.addEventListener( 'mousedown', onMouseGrab );
+  document.body.style.cursor = "grab";
+
+  intersects[0].object.position.x -= intersects[0].point.x + intersects[0].uv.x;
+  cards.grab(cardHighlighted);
+}
+
+function onMouseDrop(event) {
+  console.log("onmouseDrop", event);
+  cards.drop(event.object);
+}
+
+let canvas = document.querySelector("canvas");
+console.log("canvas", canvas);
+window.addEventListener("pointermove", onPointerMove);
+//window.addEventListener("mouseup", onMouseUp);
+window.addEventListener("click", onMouseDown);
+//window.addEventListener("mousedown", onMouseGrab);
+/* window.addEventListener("dragover", function (event) {
+  event.preventDefault();
+}); */
+//window.addEventListener("dragend", onMouseDrop);
